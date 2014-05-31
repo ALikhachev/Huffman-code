88d1d240032d5ff2529982eca5a9fd91fba7a300
Alexander Likhachev <likhachev96@gmail.com>
2014-05-31 23:43:06 +0700

    Add support to use fast-encode mode & add that information to readme.

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ Huffman's algorhytm
 
 Program can encode and decode texts using Huffmans' algorhytm.
 
-You can specify mode (e for encode or d for decode), input file and output file as arguments.
+You can specify mode (e for encode or d for decode or qe for fast encode), input file and output file as arguments.
--- main.c
+++ main.c
@@ -16,13 +16,13 @@ int main(int argc, char *argv[])
   int mode;
   char *infilename;
   char *outfilename;
-  mode = (argc > 1) ? (!strcmp(argv[1], "e") ? 0 : 1) : 0;
+  mode = (argc > 1) ? (!strcmp(argv[1], "e") ? 0 : (!strcmp(argv[1], "qe") ? 2 : 1)) : 0;
   infilename = argc > 2 ? argv[2] : "input.txt";
   outfilename = argc > 3 ? argv[3] : "output.txt";
   setlocale(LC_ALL, "");
   printf("Input file: %s\n", infilename);
   printf("Output file: %s\n", outfilename);
-  printf("Mode: %s\n\n", mode == 0 ? "encoding" : "decoding");
+  printf("Mode: %s\n\n", mode == 0 || mode == 2 ? "encoding" : "decoding");
 
   printf("%s", messages[doHuffman(infilename, outfilename, mode)]);
 
